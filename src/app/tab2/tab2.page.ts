752d59ec53ebc0b943983847070e70c4d4f588ea
ssaquisi <ssaquisi@espol.edu.ec>
2024-12-08 12:14:00 -0500

    Avance de la guía 17

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,57 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonSelect, IonSelectOption, IonTextarea, IonButton,
+  IonList, IonItem, IonLabel
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+/* Importe el servicio */
+import { ProviderService } from '../services/provider.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [ReactiveFormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonSelect, IonSelectOption, IonTextarea, IonButton,
+    IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
 
-  constructor() {}
+  /* Instancie un formulario */
+  myForm: FormGroup = new FormGroup({
+    score: new FormControl("", Validators.required),
+    opinion: new FormControl("", Validators.required)
+  });
+
+  /* Nombre de la colección */
+  collectionName = 'reviews';
+
+  /* Arreglo con datos locales */
+  dataList: any[] = [];
+
+  /* Inyecte la dependencia a Firestore */
+  constructor(private providerService: ProviderService) { }
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onSubmit() {
+    this.providerService.createDocument(this.collectionName, this.myForm.value).then(() => {
+      this.myForm.reset()
+    });
+  }
+
+  /* Al inicializar, carga los datos  */
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.providerService.readCollection(this.collectionName).subscribe((data) => {
+      this.dataList = data;
+    });
+  }
 
 }
