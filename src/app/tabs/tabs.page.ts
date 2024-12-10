45d8e4bb6a76446f656fcd30afd59c229f99c777
ssaquisi <ssaquisi@espol.edu.ec>
2024-12-10 14:50:27 -0500

    Avance de la guía 18

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, peopleCircle } from 'ionicons/icons';
+import { rocket, triangle, ellipse, square, peopleCircle } from 'ionicons/icons';
 
  
 
@@ -16,6 +16,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square, peopleCircle });
+    addIcons({ rocket, triangle, ellipse, square, peopleCircle });
   }
 }
